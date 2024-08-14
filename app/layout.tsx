f4db06dd6235877f7382de87f1df4843c68e92af
dhruv3375 <pateldhruv2351@gmail.com>
2024-08-14 20:17:03 +0530

    Layouts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,50 @@
-import React from "react";
+import type { Metadata } from "next";
+// eslint-disable-next-line camelcase
+import { Inter, Space_Grotesk } from "next/font/google";
+import { ReactNode } from "react";
 import { ClerkProvider } from "@clerk/nextjs";
-import { title } from "process";
+// import { ThemeProvider } from "@/context/ThemeProvider";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import "./globals.css";
+// import "../styles/prism.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-spaceGrotesk",
+});
+
+export const metadata: Metadata = {
+  title: "Dev Overflow",
+  description:
+    "A community-driven platform for asking and answering programming questions. Get help, share knowledge and collaborate with developers from around the world. Explore topics in web developments, mobile app development, algorithms, data structures and more...",
+  icons: {
+    icon: "/assets/images/logo.png",
+  },
+};
+
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <head>
-        <title>Next.js 13 with Clerk</title>
-      </head>
-      <ClerkProvider>
-        <body>{children}</body>
-      </ClerkProvider>
+      <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+        <ClerkProvider
+          appearance={{
+            elements: {
+              formButtonPrimary: "primary-gradient",
+              footerActionLink: "primary-text-gradient hover:text-primary-500",
+            },
+          }}
+        >
+          {/* <ThemeProvider>{children}</ThemeProvider> */}
+          {children}
+        </ClerkProvider>
+      </body>
     </html>
   );
 }
